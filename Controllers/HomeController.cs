9692c986fe5cad9b72c917d5b64f63940d65d50b
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-24 19:54:32 +0100

    use type inference and anonymous types

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,23 +10,14 @@ namespace LanguageFeatures.Controllers
   {
     public IActionResult Index()
     {
-      Product[] productArray = {
-        new Product { Name = "Kayak", Price = 275M },
-        new Product { Name = "LifeJacket", Price = 48.95M },
-        new Product { Name = "Soccer ball", Price = 19.50M },
-        new Product { Name = "Corner flag", Price = 34.95M }
+      var products = new [] { // Anonymous types
+        new { Name = "Kayak", Price = 275M },
+        new { Name = "LifeJacket", Price = 48.95M },
+        new { Name = "Soccer ball", Price = 19.50M },
+        new { Name = "Corner flag", Price = 34.95M }
       };
-      
-      decimal priceFilterTotal = productArray
-                                    .Filter(p => (p?.Price ?? 0) >= 20)
-                                    .TotalPrices();
-      decimal nameFilterTotal = productArray
-                                    .Filter(p => p?.Name?[0] == 'S')
-                                    .TotalPrices();
 
-      return View(new string[] {
-          $"Price Total: {priceFilterTotal:C2}",
-          $"Name Total: {nameFilterTotal:C2}" });
+      return View(products.Select(p => p.Name));
     }
   }
 }
