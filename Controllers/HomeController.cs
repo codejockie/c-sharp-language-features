88436be58a5396dc6312c0b5b23ad905426f0e5e
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-24 20:29:08 +0100

    use nameof operator

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using LanguageFeatures.Models;
 
@@ -9,11 +9,17 @@ namespace LanguageFeatures.Controllers
 {
   public class HomeController : Controller
   {
-    public async Task<IActionResult> Index()
+    public IActionResult Index()
     {
-      long? length = await MyAsyncMethods.GetPageLength();
+      var products = new [] { // Anonymous types
+        new { Name = "Kayak", Price = 275M },
+        new { Name = "LifeJacket", Price = 48.95M },
+        new { Name = "Soccer ball", Price = 19.50M },
+        new { Name = "Corner flag", Price = 34.95M }
+      };
 
-      return View(new string[] { $"Length: {length}" });
+      return View(products.Select(p =>
+        $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
     }
   }
 }
