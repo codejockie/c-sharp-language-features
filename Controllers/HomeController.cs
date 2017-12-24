976be173dcbbf6f8d92d20d71fa2e5c908c2eab0
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-24 20:15:57 +0100

    use async and await

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,22 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
   public class HomeController : Controller
   {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-      var products = new [] { // Anonymous types
-        new { Name = "Kayak", Price = 275M },
-        new { Name = "LifeJacket", Price = 48.95M },
-        new { Name = "Soccer ball", Price = 19.50M },
-        new { Name = "Corner flag", Price = 34.95M }
-      };
+      long? length = await MyAsyncMethods.GetPageLength();
 
-      return View(products.Select(p => p.Name));
+      return View(new string[] { $"Length: {length}" });
     }
   }
 }
